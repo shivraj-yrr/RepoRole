b6b696c6460f426f2fecd589250725e9c50b8587
shivraajjjjj <rajpootshivraj1@gmail.com>
2026-09-11 14:37:55 +0530

    removing unimportant code

--- Frontend/src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="text-center mt-10 text-gray-400">
-      Server waking up... ⏳
-    </div>
-  );
-};
-
-export default Loader;
--- Frontend/src/utils/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true, // 🔥 REQUIRED for auth cookies
-});
-
-export default instance;
